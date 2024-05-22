c2591ca4c07c98c0938fe623c9714f63a46a64f3
javavirys <javavirys@gmail.com>
2024-05-22 12:50:46 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# More details: https://udfsoft.com 
+
 ENABLED_DECODERS=(vorbis opus flac alac aac ec3 eac3 dca mlp truehd)
 HOST_PLATFORM="linux-x86_64"
 NDK_PATH="$(pwd)/android-ndk-r22b"
+SDK_PATH=$1
+# SDK_PATH=/home/user/Android/Sdk
+
+if [ -z $1  ]; then
+  echo 'Pass SDK_PATH!'
+  echo 'Example: ./build.sh /home/udfsoft/Android/Sdk'
+  exit
+fi
 
 wget https://dl.google.com/android/repository/android-ndk-r22b-linux-x86_64.zip -O ndk.zip
 unzip ./ndk.zip
@@ -38,5 +48,12 @@ cd "${FFMPEG_MODULE_PATH}/jni"
 
 ./build_ffmpeg.sh "${FFMPEG_MODULE_PATH}" "${NDK_PATH}" "${HOST_PLATFORM}" "${ENABLED_DECODERS[@]}"
 
+
 cd $EXOPLAYER_PATH
+
+echo "sdk.dir=${SDK_PATH}" > local.properties
+
 ./gradlew extension-ffmpeg:assembleRelease
+
+echo "The library is complete!"
+echo "It should be located in this path: ${EXOPLAYER_PATH}/extensions/ffmpeg/buildout/outputs/aar/"
